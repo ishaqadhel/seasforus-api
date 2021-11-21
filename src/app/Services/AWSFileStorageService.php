c4e8e8ba03fad46f66dd1ffaaae1dd0238ae1410
Muhammad Zhafran <frans.zhafran@gmail.com>
2021-11-21 15:16:22 +0700

    feat: bootstrap storage service

---
+++ src/app/Services/AWSFileStorageService.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+
+class AWSFileStorage {
+    /**
+     * Store a file to storage
+     * 
+     * @param mixed $file
+     * @param string $filename
+     * @param int $id
+     */
+    public function save($file, string $filename) {
+        $success = Storage::disk('s3')->put($filename, $file, 'public');
+
+        return $success;
+    }
+
+    /**
+     * Get disk url
+     * 
+     * @param string $disk
+     * 
+     * @return string
+     */
+    public function getDiskURL(string $disk): string {
+        return 'https://fileppdbsumsel.sgp1.digitaloceanspaces.com/';
+    }
+
+
+    /**
+     * Get url by filename
+     * 
+     * @param mixed $filename
+     * @param int $id
+     * 
+     * @return string
+     */
+    public function getUrl($filename): string {
+        return Storage::disk('s3')->url($filename);
+    }
+}
